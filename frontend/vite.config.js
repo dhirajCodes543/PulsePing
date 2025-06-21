78b80b60a2db983d9ad2ad9af2f6da1af3738bf8
dhirajCodes543 <d4dhirajbarnwal@gmail.com>
2025-06-22 01:06:13 +0530

    second commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirects',
+      generateBundle() {
+        this.emitFile({
+          type: 'asset',
+          fileName: '_redirects',
+          source: '/* /index.html 200'
+        })
+      }
+    }
+  ]
 })
